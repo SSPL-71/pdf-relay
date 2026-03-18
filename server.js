19a6100d32eb39a8e090458787391114a6517b09
SSPL71 <ssservicespl@gmail.com>
2026-03-18 20:38:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.post("/upload-statement", async (req, res) => {
 
     const folderId = req.body.folderId;
     const statement = req.body.statement;
-    
+    const role = req.body.role;
 
 
     const response = await fetch(GOOGLE_SCRIPT_URL, {
@@ -82,7 +82,8 @@ app.post("/upload-statement", async (req, res) => {
       body: JSON.stringify({
         action: "updateInsuredStmt",
         folderId: folderId,
-        transcript: statement
+        transcript: statement,
+        role: role
     })
     });
 
