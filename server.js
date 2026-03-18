5beb0b02dcb27099e80683305291cf962a0bff32
SSPL71 <ssservicespl@gmail.com>
2026-03-18 20:22:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.post("/upload-statement", async (req, res) => {
     console.log("Incoming statement upload:", req.body);
 
     const folderId = req.body.folderId;
-    const transcript = req.body.transcript;
+    const statement = req.body.statement;
     
 
 
